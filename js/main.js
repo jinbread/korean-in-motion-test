12146f73ced54e3aa3bc51c3302985912e31c1aa
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro-681.local>
2019-05-13 09:51:03 +0200

    clean codes

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $('.ml2').each(function(){
     });
 
   
-    // Wrap every letter in a span
+// Wrap every letter in a span
 $('.ml6 .letters').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
@@ -44,24 +44,6 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
-
-  anime({
-    targets: '.morphing-demo .polymorph',
-    points: [
-      { value: [
-        '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
-        '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369']
-      },
-      { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
-      { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
-      { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
-    ],
-    easing: 'easeOutQuad',
-    duration: 2000,
-    loop: true
-  });
-
-
 
 
 
@@ -105,14 +87,9 @@ const text2 = new Blotter.Text('스르륵', {
   weight: 800
 });
 
-// Use a material
-// https://blotter.js.org/#/materials
-let material2 = new Blotter.SlidingDoorMaterial();
 
-// Set material opts
-// material2.uniforms.uSpeed.value = 0.3;
-// material2.uniforms.uVolatility.value = 0.10;
-// material2.uniforms.uSeed.value = 0.1;
+// Use a material
+let material2 = new Blotter.SlidingDoorMaterial();
 
 let blotter2 = new Blotter(material2, {
   texts: text2
@@ -124,52 +101,12 @@ let elem2 = document.getElementById('blotter-sliding');
 scope2.appendTo(elem2);
 
 
-// anime({
-//   targets: '#Shape',
-//   translateX: '40',
-//   duration: 3400,
-//   loop: true,
-//   delay: anime.stagger(100)
-// })
 
 var anTl = anime.timeline({
   easing: 'easeOutExpo',
   duration: 2000,
   loop: true,  
 })
-
-// anTl
-// .add({
-//   targets: '#Shape',
-//   translateX: '50',
-//   duration: 1000,
-//   delay: anime.stagger(100)
-// })
-// .add({
-//   targets: '#Shape',
-//   translateY: '50',
-//   duration: 1000,
-//   delay: anime.stagger(100)
-// })
-// .add({
-//   targets: '#Shape',
-//   translateX: '-50',
-//   duration: 1000,
-//   delay: anime.stagger(100)
-// })
-// .add({
-//   targets: '#Shape',
-//   translateY: '-50',
-//   duration: 1000,
-//   delay: anime.stagger(100)
-// })
-// .add({
-//   targets: '#Shape',
-//   translateX: '0',
-//   translateY: '0',
-//   duration: 1000,
-//   delay: anime.stagger(100)
-// })
 
 anTl
 .add({
