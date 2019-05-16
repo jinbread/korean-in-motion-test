1e304349fe22e034310caaaf4b7ca6f7fecf4b2a
Jinbread <jinjae.lee@icloud.com>
2019-05-17 00:35:57 +0200

    h library updated to two.js

--- js/main.js
+++ js/main.js
@@ -346,66 +346,6 @@ c4Tl
 
 
 
-hTl = anime.timeline({
-  easing: 'easeInOutExpo',
-  loop: true
-})
-
-var h1Path = anime.path('#h-1-1')
-var h2Path = anime.path('#h-2-1')
-
-h2Tl = anime.timeline({
-  easing: 'easeInOutExpo',
-  loop: true
-})
-
-
-anime({
-  targets: '#h-1-2',
-  translateX: h1Path('x'),
-  translateY: h1Path('y'),
-  rotate: h1Path('angle'),
-  easing: 'easeInOutExpo',
-  duration: 1500,
-  loop: true,
-  direction: 'alternate'
-})
-
-
-anime({
-  targets: '#h-2-2',
-  translateX: h2Path('x'),
-  translateY: h2Path('y'),
-  rotate: h2Path('angle'),
-  easing: 'easeInOutExpo',
-  duration: 3000,
-  loop: true,
-  direction: 'alternate',
-});
-
-
-anime({
-  targets: '#h-1-1',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutExpo',
-  duration: 1500,
-  delay: function(el, i) { return i * 250 },
-  direction: 'alternate',
-  loop: true
-})
-
-anime({
-  targets: '#h-2-1',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutExpo',
-  duration: 3000,
-  delay: function(el, i) { return i * 250 },
-  direction: 'alternate',
-  loop: true,
-})
-
-
-
 anime({
   targets: '.svg-d',
   rotate: '1turn',
@@ -523,10 +463,6 @@ anime({
 function setup(){
   var canvas = createCanvas(300, 300);
   canvas.parent('svg-content-d');
-  // background(255, 0, 200);
-
-  
-
 }
 
 
@@ -708,3 +644,34 @@ function draw() {
   yR = yR + 1;
 
 }
+
+var elem = document.getElementById('svg-content-h');
+var params = { width: 300, height: 300 };
+var two = new Two(params).appendTo(elem);
+
+var circle = two.makeCircle(0, 0, 44);
+var circle2 = two.makeCircle(0, 0, 102);
+var circle3 = two.makeCircle(0, -44, 8);
+var circle4 = two.makeCircle(0, -102, 8);
+
+circle.noFill();
+circle.stroke = '#ffffff'; // Accepts all valid css color
+circle.linewidth = 2;
+
+circle2.noFill();
+circle2.stroke = '#ffffff'; // Accepts all valid css color
+circle2.linewidth = 2;
+
+circle3.noStroke();
+circle4.noStroke();
+
+var group1 = two.makeGroup(circle, circle3);
+group1.translation.set(150, 150);
+
+var group2 = two.makeGroup(circle2, circle4);
+group2.translation.set(150, 150);
+
+two.bind('update', function(frameCount){
+  group1.rotation += 0.01 * Math.PI;
+  group2.rotation -= 0.005 * Math.PI;
+}).play();
