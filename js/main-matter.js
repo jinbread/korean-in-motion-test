e3a264252049ff75b8538b335ab68c165be3941c
Jinbread <jinjae.lee@icloud.com>
2019-05-27 19:59:10 +0200

    physics engine updated

--- js/main-matter.js
+++ js/main-matter.js
@@ -20,12 +20,14 @@ var render = Render.create({
 // create two boxes and a ground
 var boxA = Bodies.rectangle(100, 100, 80, 80);
 var boxB = Bodies.rectangle(50, 50, 80, 80);
-var ground = Bodies.rectangle(150, 300, 500, 50, { isStatic: true });
+var boxC = Bodies.rectangle(250, 50, 120, 120);
+var boxD = Bodies.rectangle(200, 20, 40, 40);
+var ground = Bodies.rectangle(150, 307, 500, 10, { isStatic: true });
 
 
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [boxA, boxB, boxC, boxD, ground]);
 
 // run the engine
 Engine.run(engine);
